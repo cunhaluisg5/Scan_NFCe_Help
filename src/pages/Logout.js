9272bbba14d6e651e98328ef07e80d6b0f520ccf
Luis Cipriani <cunhaluisg5@gmail.com>
2020-11-13 22:20:09 -0300

    Adição de tela Sair

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import '../components/Navbar.css';
+
+import LogoutImage1 from '../assets/img/logout/01_logout.png';
+import AuthenticationImage1 from '../assets/img/authentication/01_authentication.png';
 
 export default function Logout() {
     return (
-        <div>
+        <div className='container'>
             <h1>Sair</h1>
+            <h3>1 - Efetuar logout na aplicação</h3>
+            <p>Para encerrar a sessão basta clicar no menu lateral da aplicação em "Sair"</p>
+            <img className='verticalImage' src={LogoutImage1} />
+            <h3>2 - Retorno para Autenticação</h3>
+            <p>Ao efetuar logout, a aplicação será redirecionada para a tela de autenticação.</p>
+            <img className='verticalImage' src={AuthenticationImage1} />
         </div>
     )
 }
